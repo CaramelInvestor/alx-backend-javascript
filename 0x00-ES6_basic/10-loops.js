59e44662ccde14b2d67bc2988e6cb2bd8642b7ff
CaramelInvestor <caramelInvestor@gmail.com>
2024-02-06 00:39:57 +0100

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arrayEnd = [];
-  for (const idx of array) {
-    arrayEnd.push(`${appendString}${idx}`);
+  const newArray = [];
+  for (let idx of array) {
+    newArray.push(appendString + idx);
   }
-  
-  return arrayEnd;
+
+  return newArray;
 }
