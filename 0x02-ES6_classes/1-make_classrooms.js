bc5c6b2a15bb40573f3446e768d30709e8ed710b
CaramelInvestor <caramelInvestor@gmail.com>
2024-02-08 21:42:00 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const length = [19, 20, 34];
-  return length.map((size) => new ClassRoom(size));
+  const arr = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return (arr);
 }
